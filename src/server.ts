037eac38d530a44d223f82360abe8ea6db3a7caa
HST159075 <alamtasin18@gmail.com>
2026-02-04 23:34:50 +0600

    Fix Vercel server config and 404 issue

--- src/server.ts
+++ src/server.ts
@@ -9,18 +9,20 @@ const PORT = process.env.PORT || 5000;
 async function main() {
   try {
     await prisma.$connect();
-    console.log("Connected to the database succesefully!");
+    console.log("Connected to the database successfully!");
+
     if (process.env.NODE_ENV !== "production") {
       app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
+        console.log(`Server running at http://localhost:${PORT}`);
       });
     }
   } catch (error) {
-    console.error("An error occurred: ", error);
+    console.error("An error occurred:", error);
     await prisma.$disconnect();
     process.exit(1);
   }
 }
 
 main();
+
 export default app;
